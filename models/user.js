a1674da60a28cd87f3d648f86f469a6207228b19
ruchit lukhi <ruchit.15801@gmail.com>
2026-01-07 17:30:58 +0530

    fix issue

--- models/user.js
+++ models/user.js
@@ -41,6 +41,10 @@ const userSchema = new mongoose.Schema({
         default: '+91',
         trim: true
     },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
     // Profile fields (can be updated later)
     pincode: {
         type: String,
